4c253fc0437c2f90a427bbfa34a8345053a33d8c
oppong-coffie <teama@teamalfy.com>
2024-11-09 23:25:50 +0000

    remane component promo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Promo from './components/promo'
+import Promo from './components/Promo.jsx'
 
 function App() {
 
